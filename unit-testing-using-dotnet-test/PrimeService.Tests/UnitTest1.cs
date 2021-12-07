f5d953a79ca3ec0f9395d4e8884029f7dfa2a7ee
radammu <radammu@tesla.com>
2021-12-06 22:11:09 -0800

    update

--- unit-testing-using-dotnet-test/PrimeService.Tests/UnitTest1.cs
+++ unit-testing-using-dotnet-test/PrimeService.Tests/UnitTest1.cs
@@ -7,13 +7,18 @@ namespace PrimeService.Tests
     public class UnitTest1
     {
 
-
         [TestMethod()]
         public void IsPrime_InputIs1_ReturnFalse()
         {
             var primeService = new PrimeServices();
             bool result = primeService.IsPrime(1);
 
+            Assert.IsFalse(false, "1 should not be prime");
+        }
+
+        [TestMethod()]
+        public void IsPrime_InputIs1_ReturnFalse_Test()
+        {            
             Assert.IsFalse(false, "1 should not be prime");
         }
     }
